82264ec4cb34344aa1512469fdc5ae4a074d8507
chrispychrisx <you@example.com>
2025-03-03 14:35:56 +0100

    just new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,15 @@ import heroImg from '../assets/hero/bg.jpg'
 
 const Hero = () => {
   return (
-    <section className='hero h-[640px] bg-center bg-no-repeat bg-fixed relative z-20 mt-14 lg:rounded-bl-[290px]' 
+    <section className='hero h-[640px] bg-center bg-no-repeat bg-fixed relative z-20 mt-7 lg:rounded-bl-[290px]' 
         style={{ backgroundImage: `url(${heroImg})` }}>
         <div className="container mx-auto h-full flex items-center justify-center">
             <div className="hero__text w-[567px]">
-                <h1 className='text-[2.3rem] md:text-[3.5rem] font-semibold mb-8'>Let Your Home Be Unique</h1>
+                <h1 className='text-[2.3rem] md:text-[3.5rem] sm:text-[2.3rem] font-semibold mb-8 text-center'>Let Your Home Be Unique</h1>
+
+                <p className='mb-8 text-[1.1rem] text-center'>
+                  There are many variations of the passages of lorem Ipsum from available, variations of the passages.
+                </p>
                 
                 <button className='btn btn-primary mx-auto lg:mx-0'>
                     Get free estimation
